e183a6590ffef37e3765a6af47dd4444a96d8a53
jakenjarvis <jaken.jarvis@gmail.com>
2020-08-23 17:52:07 +0900

    Added character dialogue.

--- Lakshmi.py
+++ Lakshmi.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import os
+import random
 import traceback
 
 import discord
@@ -19,6 +20,17 @@ extensions = [
 for extension in extensions:
     bot.load_extension(extension)
 
+character_command_not_found_dialogue = [
+    "…？　今のは、なんだろう？　…ところで今、アップルパイを焼いてるの、おひとついかが？",
+    "…？　( ˘ω˘)ｽﾔｧ…あっ、いけない…寝てた…。なんか今、言われた気がするけど…なんだろう…？",
+    "…あれ、私の…林檎（ｷｮﾛｷｮﾛ）…何か言われた気がするけど…何だろう？　…それはさておき、林檎林檎（ｷｮﾛｷｮﾛ）",
+    "…？　何か言われた気がするけど…？　きょ…今日から深夜アニメに挑戦よ…。…どれを見ようかすごく悩む…。",
+    "あっ、このクマのぬいぐるみ…これは買いね……。今、何か言われた気がするけど…。あぁ…、このクマ最高……♡…( ˘ω˘)ｽﾔｧ",
+    "何かわからなかったせいで、私は今、ヨガみたいなポーズをとらされているわ……ダイスなんて振れない！",
+    "…今のは…？　今『くとぅるふ』の『るーるぶっく』を読んでるわ……。貴方達もやってる！？…私も探索したい…。",
+    "…今のは何だろう…？　焼きリンゴも案外悪くないわね……あつ…。今度、貴方達にもおすそ分けしたい…。",
+    "…今のは何だろう…？　私がファンブル連発したら『おはシュミ』と、いわれるのかしら…。私がファンブルの具現化なんて…やだ…。",
+]
 
 @bot.listen()
 async def on_message(message):
@@ -33,7 +45,8 @@ async def on_command_error(context, error):
     # await self.bot.on_command_error(context, PermissionNotFound())
 
     if isinstance(error, commands.CommandNotFound):
-        await context.send(f'{context.author.mention} ごめんなさい。何のことかよくわからなかったわ。')
+        message = random.choice(character_command_not_found_dialogue)
+        await context.send(f'{context.author.mention} {message}')
 
     elif isinstance(error, PermissionNotFound):
         await context.send(f'{context.author.mention} あなたにはコマンドを実行する権限が無いようよ。')
