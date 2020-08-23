aeb46cbaa24901cb028f1e47cc4cc9b224484895
jakenjarvis <jaken.jarvis@gmail.com>
2020-08-23 16:52:28 +0900

    Improved message handling  - Fixed the problem that command judgment failed in the second and subsequent lines  - Modified to round up the calculated total value  - Added normalization processing of commands  - Added comment processing  - Added comparison judgment processing  - Added critical and fumble processing

--- Lakshmi.py
+++ Lakshmi.py
@@ -9,7 +9,7 @@ from discord.ext import commands
 from LakshmiStorage import LakshmiStorage
 from LakshmiErrors import PermissionNotFound
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix=':')
 bot.storage = LakshmiStorage()
 
 
@@ -20,16 +20,23 @@ for extension in extensions:
     bot.load_extension(extension)
 
 
+@bot.listen()
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if bot.user in message.mentions: # 話しかけられたかの判定
+        await message.channel.send(f'{message.author.mention} ん？わたくしを呼びました？')
+
 @bot.event
 async def on_command_error(context, error):
     # Throwしたいときは、以下のようにon_command_errorを呼び出す。
     # await self.bot.on_command_error(context, PermissionNotFound())
 
     if isinstance(error, commands.CommandNotFound):
-        await context.send('ごめんなさい。何のことかよくわからなかったわ。')
+        await context.send(f'{context.author.mention} ごめんなさい。何のことかよくわからなかったわ。')
 
     elif isinstance(error, PermissionNotFound):
-        await context.send('あなたにはコマンドを実行する権限が無いようよ。')
+        await context.send(f'{context.author.mention} あなたにはコマンドを実行する権限が無いようよ。')
 
     else:
         original_error = getattr(error, "original", error)
--- cogs/DiceBotCog.py
+++ cogs/DiceBotCog.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import os
 import re
+import math
 import random
 
 import mojimoji
@@ -17,16 +18,16 @@ def replace_dice_string(match):
     return result
 
 def replace_display_string(calculation):
-    result = calculation
-    result = result.replace('+','＋')
-    result = result.replace('-','－')
-    result = result.replace('*','×')
-    result = result.replace('/','÷')
-    return result
+    return calculation.replace('+','＋').replace('-','－').replace('*','×').replace('/','÷')
 
 class DiceBotCog(commands.Cog):
-    VALID_CHARACTERS = re.compile(r"^([-+*/a-zA-Z0-9)(<>= ])+$", re.IGNORECASE)
-    VALID_DICE = re.compile(r"((\d+)d(\d+))", re.IGNORECASE)
+    DICE_ROLL_COMMAND = ":f"
+    REPLACE_COMMAND = re.compile(r"(([:：])([fFｆＦ]))")
+    REPLACE_DICE = re.compile(r"((\d+)d(\d+))", re.IGNORECASE)
+    VALID_CHARACTERS = re.compile(r"^([-+*/a-zA-Z0-9\.)(<>= ])+$", re.IGNORECASE)
+    VALID_COMMENT = re.compile(r"(#|＃)")
+    VALID_COMPARISON = re.compile(r"([><=]+)", re.IGNORECASE)
+    VALID_PROCESS_CRITICAL = re.compile(r"^\dd100([=><]+[-+*/0-9\.]+)?$", re.IGNORECASE)
 
     def __init__(self, bot):
         self.bot = bot
@@ -37,33 +38,100 @@ class DiceBotCog(commands.Cog):
         await context.send('やっほー')
 
     @commands.command()
-    async def cf(self, context, *, command):
+    async def f(self, context, *, command):
         print(command)
         processingFlag = False
         try:
-            # TODO: コマンドの連結と再分割処理
-            # TODO: ＃コメント処理
-            # TODO: ２行目以降の処理がおかしい/cfが入ってる、消す処理追加
             # TODO: 返却文字数のチェック
-            # TODO: トータルの小数点端数切り上げ
-            commandlist = command.splitlines()
+            command_line = ""
+            comment = ""
+            dice_command = ""
+            conditional_expression = ""
+            comparison_value = ""
+
+            reply_message = []
+            reply_message.append(f'{context.author.mention}')
+            normalize_commands = self.normalize_commands(command)
+            commandlist = normalize_commands.splitlines()
             for oneCommand in commandlist:
-                fixedOneCommand = mojimoji.zen_to_han(str(oneCommand)).replace(' ','')
+                # コメント判定
+                match = DiceBotCog.VALID_COMMENT.search(oneCommand)
+                if match:
+                    # コメント有り
+                    split_command = DiceBotCog.VALID_COMMENT.split(oneCommand, maxsplit=1)
+                    command_line = split_command[0].strip()
+                    comment = split_command[2].strip()
+                else:
+                    # コメント無し
+                    command_line = oneCommand.strip()
+                    comment = ""
+
+                # コマンドの整理
+                fixedOneCommand = mojimoji.zen_to_han(command_line).replace(' ','')
                 # 安全性のチェック
-                check = DiceBotCog.VALID_CHARACTERS.search(str(fixedOneCommand))
+                check = DiceBotCog.VALID_CHARACTERS.search(fixedOneCommand)
                 if check:
-                    calculation = DiceBotCog.VALID_DICE.sub(replace_dice_string, str(fixedOneCommand))
-                    total = eval(calculation)
+                    # 大小判定処理
+                    match = DiceBotCog.VALID_COMPARISON.search(fixedOneCommand)
+                    if match:
+                        # 大小記号有り
+                        split_command = DiceBotCog.VALID_COMPARISON.split(fixedOneCommand)
+                        if len(split_command) == 3:
+                            dice_command = split_command[0].strip()
+                            conditional_expression = split_command[1].replace('=>','>=').replace('=<','<=').strip()
+                            comparison_value = split_command[2].strip()
+                        else:
+                            raise commands.CommandNotFound()
+                    else:
+                        # 大小記号無し
+                        dice_command = fixedOneCommand
+
+                    # ダイス本処理
+                    calculation = DiceBotCog.REPLACE_DICE.sub(replace_dice_string, dice_command)
+                    total = math.ceil(eval(calculation)) # 小数点切り上げ
                     displayOneCommand = replace_display_string(fixedOneCommand).lower()
                     displayCalculation = replace_display_string(calculation)
-                    await context.send(f'{context.author.mention} {displayOneCommand} = {displayCalculation} = {str(total)}')
+
+                    # 大小判定
+                    judgment_result = ""
+                    if conditional_expression != "":
+                        judgment_string = str(total) + conditional_expression + comparison_value
+                        if eval(judgment_string):
+                            judgment_result = " | Success!"
+                        else:
+                            judgment_result = " | Failure!"
+
+                    # クリティカル判定（D100のみ）
+                    critical_result = ""
+                    match = DiceBotCog.VALID_PROCESS_CRITICAL.search(fixedOneCommand)
+                    if match:
+                        # D100 only
+                        if total <= 5:
+                            critical_result = " 【 Critical! 】"
+                        elif total >= 96:
+                            critical_result = " 【  Fumble!  】"
+
+                    reply_message.append(f'{displayOneCommand} {comment} = {displayCalculation} = {str(total)}{judgment_result}{critical_result}')
                     processingFlag = True
 
-            if not processingFlag:
+            if processingFlag:
+                await context.send("\n".join(reply_message))
+            else:
                 await self.bot.on_command_error(context, commands.CommandNotFound())
 
         except Exception as e:
             await self.bot.on_command_error(context, commands.CommandNotFound())
+
+    def normalize_commands(self, command):
+        result = ""
+        # コメントがあるのでここでは.lower()しない。
+        linking_command = "🎲".join(command.splitlines())
+        convert_zenkaku = DiceBotCog.REPLACE_COMMAND.sub(DiceBotCog.DICE_ROLL_COMMAND, linking_command).replace('　',' ')
+        split_command = re.split(f'{DiceBotCog.DICE_ROLL_COMMAND}|🎲', convert_zenkaku, flags=re.IGNORECASE)
+        removal_blank_line = [row.strip() for row in split_command if row.strip() != ""]
+        result = "\n".join(removal_blank_line)
+        print(result)
+        return result
 
 def setup(bot):
     bot.add_cog(DiceBotCog(bot))
